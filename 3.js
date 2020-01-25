1dc599ba8e6e8fe3935b313b8f376231ed3e339e
adiamahardika <adiamahardika.work@gmail.com>
2020-01-25 17:35:37 +0700

    Menambahkan komentar

--- 3.js
+++ 3.js
@@ -1,14 +1,20 @@
 function count_handshake(jmlOrang){
+    //deklarasi variabel i yang berisi 0
     let i = 0
+    //deklarasi variabel jabatTangan yang berisi 0
     let jabatTangan = 0
 
+    //perulangan untuk menghitung jumlah jabat tangan
     while(i < jmlOrang){
+        
         jabatTangan +=i
+        
         i++
     }
 
     return jabatTangan
 }
 
-// console.log(count_handshake(3))
+console.log(count_handshake(3))
+
 console.log(count_handshake(5))
